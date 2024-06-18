f14c668abab4afdeb65e6d05b749c63d4edf4153
Nisa Fatima <nisafatima0000@gmail.com>
2024-06-18 14:21:15 +0500

    Initial Commit

---
+++ firebaseAuthPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  firebaseAuthPractice
+//
+//  Created by Macbook Pro on 18/06/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
